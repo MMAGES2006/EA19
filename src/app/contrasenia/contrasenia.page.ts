08c402aa4a7f5947556e2fdab5a919a25e7812a3
MMAGES2006 <22170@portalcedi.edu.mx>
2025-03-18 08:29:59 -0600

    paginas entre otros

---
+++ src/app/contrasenia/contrasenia.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contrasenia',
+  templateUrl: './contrasenia.page.html',
+  styleUrls: ['./contrasenia.page.scss'],
+})
+export class ContraseniaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
