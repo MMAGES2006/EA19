58698b189d07383ed5010c57359f3be3a89e86db
B) <22089@portalcedi.edu.mx>
2025-03-18 08:38:45 -0600

    registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController, private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  email: string = '';
+  password: string = '';
+
+  async onSubmit() {
+    try {
+      await this.authService.register(this.email, this.password);
+      const alert = await this.alertController.create({
+        header: 'Signup Success',
+        message: 'You have created successfully',
+        buttons: ['OK'],
+      });
+      await alert.present();
+      this.router.navigate(['/login']);
+    } catch (error) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Please complete all',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
+  }
+  validateEmail(email: string): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+
+  onSignUp() {
+    this.router.navigateByUrl("sign-up")
   }
 
 }
